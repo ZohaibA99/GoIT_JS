5595ecd851eec90b310d8b1630468b870fc42729
Zohaib Asif <118318498+ZohaibA99@users.noreply.github.com>
2023-05-12 00:33:14 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,92 +1,116 @@
-// const list = document.getElementById('list');
-// if(list){
-//     console.log(list.children.length);
-// } else{
-//     console.log('This element does not exist in the DOM');
+// const vanillaOutput = document.querySelector(".output.vanilla");
+// const throttledOutput = document.querySelector(".output.throttled");
+// const debounceOutput = document.querySelector('.output.debounced');
+
+// const eventCounter = {
+//     vanilla: 0, 
+//     throttled: 0,
+//     debounced: 0,
+// };
+
+// document.addEventListener("scroll", () => {
+//     eventCounter.vanilla += 1;
+//     vanillaOutput.textContent = eventCounter.vanilla;
+// })
+
+// document.addEventListener("scroll", _.throttle(() => {
+//     eventCounter.throttled += 1;
+//     console.log("event occoured");
+//     throttledOutput.textContent = eventCounter.throttled;
+// }, 300));
+
+// document.addEventListener("scroll", _.debounce(() => {
+//     eventCounter.debounced += 1;
+//     debounceOutput.textContent = eventCounter.debounced;
+// }, 300, {
+//     leading: true,
+//     trailing: false,
+// }));
+
+
+
+
+
+//In Class Exercises
+//set throttle for scrolling up and down window
+// const outputStandard = document.querySelector("#outputScrollStandart");
+// const outputThrottled = document.querySelector("#outputScrollThrottling")
+
+// const eventScrollCounter = {
+//     standard: 0,
+//     throttled: 0,
+// };
+
+// document.addEventListener("scroll", () => {
+//     eventScrollCounter.standard += 1;
+//     outputStandard.textContent = eventScrollCounter.standard;
+// });
+
+// document.addEventListener('scroll', _.throttle(() => {
+//     eventScrollCounter.throttled += 1;
+//     outputThrottled.textContent = eventScrollCounter.throttled;
+// }, 1500));
+
+
+
+//!================================
+//Set throttel for screen window resize
+
+// const outputResizeStandard = document.querySelector("#outputResizeStandart");
+// const outputResizeThrottled = document.querySelector("#outputResizeThrottling");
+
+// const eventResizeCounter = {
+//     standard: 0,
+//     throttled: 0,
+// };
+
+// window.addEventListener("resize", () => {
+//     eventResizeCounter.standard += 1;
+//     outputResizeStandard.textContent = eventResizeCounter.standard;
+// });
+
+// window.addEventListener("resize", _.throttle(() => {
+//     eventResizeCounter.throttled += 1;
+//     outputResizeThrottled.textContent = eventResizeCounter.throttled;
+// }, 1500))
+
+
+
+
+//!================================
+//throttle and debounce
+// const nameInput = document.getElementById("nameInput");
+
+// nameInput.addEventListener(
+//     "input",
+//     _.debounce(handleInputDebounce, 1000)
+// );
+
+// function handleInputDebounce({ target }) {
+//     console.log(target.value);
 // }
 
-//* 1. Create a list of technologies studied in the Fullstack course using js
-// Show how it is done with the help of **map** and **reduce**
 
-const technologies = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+//!================================
+const CORRECT_INPUT_LENGTH ={
+    min: 3,
+    max: 15,
+};
 
-//gets a reference (connection) to our div element with id of container 
-const container = document.getElementById('container');
+const CLASS_NAMES = {
+    wrong: "wrong",
+    correct: "correct",
+};
 
-//creating ul (remeber it has not been added to the DOM yet)
-const list = document.createElement('ul');
+const signInForm = document.getElementById("signInForm");
 
-//mapFunction
-
-// const listContent = technologies.map((tech) => `<li>${tech}</li>`).join("");
-// list.insertAdjacentHTML("beforeend", listContent);
-// list.innerHTML = listContent;
-
-// container.append(list);
-
-
-
-//reduce function
-const listContent = technologies.reduce((acc, techonolgy) => (acc += `<li>${techonolgy}</li>`), "");
-
-//add li to ul (remember they will not be displayed until you add the ul to the DOM)
-list.insertAdjacentHTML("beforeend", listContent);
-
-//adde ul to dom
-container.append(list);
-
-
-//default way
-
-/*
-    1. Create a container for ul tag
-    2. Create a cycle for sorting array elements
-    3. Create a li tag
-    4. Write the text from the array into the li tag
-    5. Place li inside of ul
-    6. Place ul tag with items on the page
-*/
-
-// const list = document.createElement('ul');
-// for (const tech of technologies){
-//     const liEl = document.createElement('li');
-//     liEl.textContent = tech;
-//     list.append(liEl);
-// }
-// container.append(list);
-
-///2. Create several buttons based on an array of objects using createElement
-
-const createTheseButtons = [
-    {
-        label: 'red',
-        color: "#FF0000"
-    },
-    {
-        label: 'blue',
-        color: "#0000FF"
-    },
-    {
-        label: "green",
-        color: "#00FF00"
-    }
-
-];
-
-
-/*
-    1. Iterate through the array of colors using a loop
-    2. Create a button
-    3. Set the background color of the button
-    4. Set the text of the button
-    5. Add a button to the document
-*/
-
-createTheseButtons.forEach(({label, color}) => {
-    const buttonEl = document.createElement('button');
-    buttonEl.style.backgroundColor = color;
-    buttonEl.classList.add("indents");
-    buttonEl.style.margin = '5px';
-    buttonEl.textContent = label;
-    container.appendChild(buttonEl);
-})
+signInForm.addEventListener("input", _.debounce(({target}) => {
+    if(target.value.length < CORRECT_INPUT_LENGTH.min || 
+        target.value.length > CORRECT_INPUT_LENGTH.max){
+            target.classList.add(CLASS_NAMES.wrong);
+            target.classList.remove(CLASS_NAMES.correct);
+        } else{
+            target.classList.add(CLASS_NAMES.correct);
+            target.classList.remove(CLASS_NAMES.wrong);
+        }
+}))
