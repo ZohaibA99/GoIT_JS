9198e6e5f1963317bb79f5cd1fe3c1bb2b602ed9
Zohaib Asif <118318498+ZohaibA99@users.noreply.github.com>
2023-05-12 00:32:33 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,92 +1,92 @@
-// const list = document.getElementById('list');
-// if(list){
-//     console.log(list.children.length);
-// } else{
-//     console.log('This element does not exist in the DOM');
+// const parentDiv = document.querySelector("#parentDiv");
+// const childId = document.querySelector("#childA");
+
+// document.body.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     alert("body");
+// })
+
+
+// parentDiv.addEventListener('click', (e) =>{
+//     alert('parent div')
+// });
+
+// childA.addEventListener('click', (e) => {
+//     e.stopPropagation();
+//     e.preventDefault();
+//     alert("child A");
+// })
+
+
+
+//!=============================================================
+
+// for (let elem of document.querySelectorAll("*")){
+//     elem.addEventListener('click', (event) =>
+//         alert(`Diving: ${elem.tagName}`),
+//         true
+//     );
+//     elem.addEventListener('click', (e) => alert(`Emersioin: ${elem.tagName}`));
 // }
 
-//* 1. Create a list of technologies studied in the Fullstack course using js
-// Show how it is done with the help of **map** and **reduce**
-
-const technologies = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
-
-//gets a reference (connection) to our div element with id of container 
-const container = document.getElementById('container');
-
-//creating ul (remeber it has not been added to the DOM yet)
-const list = document.createElement('ul');
-
-//mapFunction
-
-// const listContent = technologies.map((tech) => `<li>${tech}</li>`).join("");
-// list.insertAdjacentHTML("beforeend", listContent);
-// list.innerHTML = listContent;
-
-// container.append(list);
 
 
 
-//reduce function
-const listContent = technologies.reduce((acc, techonolgy) => (acc += `<li>${techonolgy}</li>`), "");
+// Create a product cost calculator depending on the price. When working with events, use delegation. Render all markup via JS
+// When the data variable is entered in input #1 or #2, the total cost should be automatically recalculated
+// The currency - USD should always be displayed
+// The total amount is displayed with pennies (2 decimal places)
+// In the signature of the second input, there should be a color hint of how many pounds the user selected on the second input.
+// The color tooltip should change its value when the slider is dragged.
+// In input No. 2, set min and max threshold by yourself.
+// When loading the page, the script should automatically calculate the cost based on the default data that you set in the markup.
 
-//add li to ul (remember they will not be displayed until you add the ul to the DOM)
-list.insertAdjacentHTML("beforeend", listContent);
 
-//adde ul to dom
-container.append(list);
-
-
-//default way
-
-/*
-    1. Create a container for ul tag
-    2. Create a cycle for sorting array elements
-    3. Create a li tag
-    4. Write the text from the array into the li tag
-    5. Place li inside of ul
-    6. Place ul tag with items on the page
-*/
-
-// const list = document.createElement('ul');
-// for (const tech of technologies){
-//     const liEl = document.createElement('li');
-//     liEl.textContent = tech;
-//     list.append(liEl);
-// }
-// container.append(list);
-
-///2. Create several buttons based on an array of objects using createElement
-
-const createTheseButtons = [
-    {
-        label: 'red',
-        color: "#FF0000"
+const data = {
+    quantity: 0,
+    price: 0,
+    calcTotalPrice() {
+        return (this.quantity * this.price).toFixed(2);
     },
-    {
-        label: 'blue',
-        color: "#0000FF"
-    },
-    {
-        label: "green",
-        color: "#00FF00"
+};
+
+//dom links
+const mainForm = document.getElementById("form");
+const priceInput = document.getElementById("price");
+const quantityInput = document.getElementById("quantity");
+const amount = document.getElementById("amount");
+const total = document.getElementById("total");
+
+//data fill
+dataFill();
+displayTotal();
+
+//events
+mainForm.addEventListener("change", handleFormChange);
+
+//functions
+function handleFormChange({ target }) {
+    const { value } = target;
+
+    if (target === priceInput) {
+        if (!Number(value)) {
+        console.log("Not a number!");
+        return;
+        }
+    } else if (target === quantityInput) {
+        amount.textContent = value;
     }
 
-];
+    target.setAttribute("value", value);
+    dataFill();
+    displayTotal();
+}
 
+function dataFill() {
+    data.quantity = quantityInput.getAttribute("value");
+    data.price = priceInput.getAttribute("value");
+}
 
-/*
-    1. Iterate through the array of colors using a loop
-    2. Create a button
-    3. Set the background color of the button
-    4. Set the text of the button
-    5. Add a button to the document
-*/
-
-createTheseButtons.forEach(({label, color}) => {
-    const buttonEl = document.createElement('button');
-    buttonEl.style.backgroundColor = color;
-    buttonEl.classList.add("indents");
-    buttonEl.style.margin = '5px';
-    buttonEl.textContent = label;
-    container.appendChild(buttonEl);
-})
+function displayTotal() {
+    total.textContent = data.calcTotalPrice() + " USD";
+}
